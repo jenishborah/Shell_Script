616ac6f1a8b1b8bffd4eea7d3adff99c182d2c0b
jenishborah <jenishborah@gmail.com>
2023-06-01 20:59:39 +0530

    First commit

---
+++ scriptq1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+a=10
+b=20
+
+if [ $a == $b ]
+then 
+	echo "a and b is same"
+else 
+	echo "a and b is not same"
+fi
+
---
+++ scriptq2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+echo "enter the first  value= "
+read a
+echo "$a"
+echo "enter the first  value= "
+read b
+echo "$b"
+
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+elif [ $a -gt $b ]
+then
+   echo "a is greater than b"
+elif [ $a -lt $b ]
+then
+   echo "a is less than b"
+else
+   echo "None of the condition met"
+fi
