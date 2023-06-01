cadaab80d1a147772b1f30a1adfb138a16b9b5d7
Debasish Dutta <ddmasterdon@gmail.com>
2023-06-01 21:59:04 +0530

    Create alias.sh

---
+++ alias.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alias l='ls -al'
+alias godocs='cd /usr/share/doc'
+alias cls='clear'
+alias sysinfo='echo "System Information: $(uname -a)"'
+alias checknet='ping -c 4 google.com'
+alias diskspace='df -h'
+alias glog='git log --oneline --graph --decorate'
+alias fsearch='find . -type f -name'
+alias cloc='find . -type f -name "*.py" -exec cat {} \; | wc -l'
